400fda42344cb9baf9011ccfda9e6e07bb599cc4
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-12-07 19:49:08 +0700

    feat: add sample components and pages

--- app/components/root/CatchBoundary.tsx
+++ app/components/root/CatchBoundary.tsx
@@ -1,5 +1,16 @@
+import {
+  Box,
+  Button,
+  Heading,
+  Image,
+  Link as ChakraLink,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
 import type { CatchBoundaryComponent } from "@remix-run/react/routeModules";
-import { useCatch } from "remix";
+import { Link, useCatch } from "remix";
+
+import MotionBox from "../motion/Box";
 
 import Document from "./Document";
 import Layout from "./layout";
@@ -7,6 +18,7 @@ import Layout from "./layout";
 // https://remix.run/docs/en/v1/api/conventions#catchboundary
 const CatchBoundary: CatchBoundaryComponent = () => {
   const caught = useCatch();
+  const { colorMode } = useColorMode();
 
   let message;
   switch (caught.status) {
@@ -35,6 +47,39 @@ const CatchBoundary: CatchBoundaryComponent = () => {
           {caught.status}: {caught.statusText}
         </h1>
         {message}
+        <MotionBox
+          animate={{ y: 20 }}
+          transition={{ repeat: Infinity, duration: 2, repeatType: "reverse" }}
+          width={["100%", "70%", "60%", "60%"]}
+          margin="0 auto"
+        >
+          <Image
+            src="/404 Error-pana.svg"
+            alt="Error 404 not found Illustration"
+          />
+        </MotionBox>
+        <Text textAlign="center" fontSize="xs">
+          <ChakraLink href="https://stories.freepik.com/web" isExternal>
+            Illustration by Freepik Stories
+          </ChakraLink>
+        </Text>
+
+        <Box marginY={4}>
+          <Heading textAlign="center">Page not Found.</Heading>
+
+          <Box textAlign="center" marginTop={4}>
+            <Text>It&apos;s Okay!</Text>
+            <Link to="/">
+              <Button
+                backgroundColor={
+                  colorMode === "light" ? "gray.300" : "teal.500"
+                }
+              >
+                Let&apos;s Head Back
+              </Button>
+            </Link>
+          </Box>
+        </Box>
       </Layout>
     </Document>
   );
--- app/root.tsx
+++ app/root.tsx
@@ -14,7 +14,13 @@ import globalStylesUrl from "~/styles/global.css";
 
 // https://remix.run/api/app#links
 export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: globalStylesUrl }];
+  return [
+    { rel: "stylesheet", href: globalStylesUrl },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css?family=Outfit",
+    },
+  ];
 };
 
 // https://remix.run/api/conventions#default-export
