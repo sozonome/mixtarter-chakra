9fcb77dea067820188b567c112c2625d0dbe7313
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-12-11 14:45:42 +0700

    feat: add 404 button

--- app/components/sample/SomeText.tsx
+++ app/components/sample/SomeText.tsx
@@ -1,9 +1,13 @@
 import {
   Box,
+  Button,
   Heading,
+  HStack,
+  Stack,
   useBreakpointValue,
   useColorMode,
 } from "@chakra-ui/react";
+import { Link } from "remix";
 
 const SomeText = () => {
   const { colorMode } = useColorMode();
@@ -13,7 +17,7 @@ const SomeText = () => {
   });
 
   return (
-    <>
+    <Stack gap={2}>
       <Heading as="h2" fontSize="3xl">
         Hello
       </Heading>
@@ -27,7 +31,13 @@ const SomeText = () => {
           This is a Remix app with Chakra-UI and TypeScript setup.
         </Box>
       </Box>
-    </>
+
+      <HStack>
+        <Link to="/404">
+          <Button>404 Page</Button>
+        </Link>
+      </HStack>
+    </Stack>
   );
 };
 
